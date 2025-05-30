012e43a18f3a2c46cf6bb21c9b0227963e722594
Tecuapacho-Martin <127567868+Tecuapacho-Martin@users.noreply.github.com>
2025-05-30 09:41:20 -0600

    Create Triangulo_funciones.py

---
+++ Triangulo_funciones.py
@@ -0,0 +1,8 @@
+
+def area_triangulo():
+    base = float(input("Ingresa la base del triangulo: "))
+    altura = float(input("Ingresa la altura del triangulo: "))
+    area = base * altura / 2
+    print("El area del triangulo es:", area)
+
+area_triangulo()
